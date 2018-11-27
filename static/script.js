bdf0eeb0b451fdeb64488bbcce36648ec310a372
Lukas Pfahler <lukas.pfahler@tu-dortmund.de>
2018-11-27 13:21:47 +0100

    well log plots dont work out of the box

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ function initHistogram(json,tag)
 	
 	
 	var binInc = json[0].data[1].bin;
-	var maxBin = json[0].data[json[i].data.length-1].bin;
+	var maxBin = json[0].data[json[0].data.length-1].bin;
 	var binInc = json[0].data[1].bin-json[0].data[0].bin;
 	//console.log(maxBin,binInc);
 	var i = 0;
